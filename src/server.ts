ac8c491d976ecbfd4cf4690a965a9d17e7134039
Smey <roemreaksmey7@gmail.com>
2024-09-09 15:59:03 +0700

    config PORT

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,14 @@
 import app from "./app";
 import connectToMongoDB from "./database/connection";
+
+//config port
 import configs from "./config";
 
 async function run() {
   try {
     await connectToMongoDB();
     app.listen(configs.port, () => {
-      console.log(`Server running on port ${configs.port}`);
+      console.log(`Server running on port ${configs.port}...`);
     });
   } catch (error) {
     console.error(error);
