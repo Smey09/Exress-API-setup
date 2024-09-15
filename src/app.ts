8a58458195b3ccfd09ea8bfb892f9fcad051b493
Smey <roemreaksmey7@gmail.com>
2024-09-15 15:42:12 +0700

    release V2.0.1

--- src/app.ts
+++ src/app.ts
@@ -4,11 +4,14 @@ import fs from "fs";
 import path from "path";
 import { RegisterRoutes } from "./routes/v1/routes";
 import { globalErrorHandler } from "./middlewares/global-error";
+import mongoose from "mongoose";
 
 // Dynamically load swagger.json
 const swaggerDocument = JSON.parse(
   fs.readFileSync(path.join(__dirname, "docs/swagger.json"), "utf8")
 );
+
+mongoose.set("debug", true);
 
 // ========================
 // Initialize App Express
